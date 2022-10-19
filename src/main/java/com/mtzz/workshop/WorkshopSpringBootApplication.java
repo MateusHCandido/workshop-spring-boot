16b2188e2d229a6e5f4d535dac2290e50477a724
Mateus Henrique <mateushcandido5@gmail.com>
2022-10-18 22:40:13 -0300

    primeiro commit: projeto criado

---
+++ src/main/java/com/mtzz/workshop/WorkshopSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mtzz.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtzz/workshop/WorkshopSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtzz.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
