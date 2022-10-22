571ad57327ba63847c6d66e41e0d093d1fdc0889
Mateus Henrique <mateushcandido5@gmail.com>
2022-10-22 12:28:46 -0300

    Criação da entidade Post com Usuario aninhado

--- src/main/java/com/mtzz/workshop/config/Instatiation.java
+++ src/main/java/com/mtzz/workshop/config/Instatiation.java
@@ -1,12 +1,16 @@
 package com.mtzz.workshop.config;
 
+import com.mtzz.workshop.domain.Post;
+import com.mtzz.workshop.repositories.PostRepository;
 import com.mtzz.workshop.repositories.UsuarioRepository;
 import com.mtzz.workshop.domain.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instatiation implements CommandLineRunner {
@@ -14,14 +18,26 @@ public class Instatiation implements CommandLineRunner {
     @Autowired
     UsuarioRepository user;
 
+    @Autowired
+    PostRepository post;
+
     @Override
     public void run(String... args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         user.deleteAll();
 
         Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com");
         Usuario u2 = new Usuario (null, "Lucas Fernando", "lucas@gmail.com");
         Usuario u3 = new Usuario (null, "Mateus Henrique", "mateush@gmail.com");
         user.saveAll(Arrays.asList(u1, u2, u3));
+
+        Post p1 = new Post(null,sdf.parse("22/10/2022"), "Concluindo curso", "Hoje concluo o curso!", u3);
+        Post p2 = new Post(null, sdf.parse("22/10/2022"),"Novo ciclo de estudos"
+                ,"Ainda hoje iniciando curso de HTML, CSS e JS", u3);
+
+        post.saveAll(Arrays.asList(p1, p2));
 
     }
 }
