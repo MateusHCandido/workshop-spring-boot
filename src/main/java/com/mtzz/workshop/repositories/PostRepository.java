8bdfc0d69bd3de29017a76f78fd7cc7687d03117
Mateus Henrique <mateushcandido5@gmail.com>
2022-10-22 17:23:23 -0300

    consultas com Query methods

--- src/main/java/com/mtzz/workshop/repositories/PostRepository.java
+++ src/main/java/com/mtzz/workshop/repositories/PostRepository.java
@@ -2,10 +2,14 @@ package com.mtzz.workshop.repositories;
 
 import com.mtzz.workshop.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
